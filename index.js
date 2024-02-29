efbb035ffad4985cb900f4c51c440b70aab8d584
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-29 13:23:24 +0100

    feat: add endpoint to fetch mockend

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ app.get("/", (request, response) => {
   response.send("Hello World");
 });
 
+app.get("/api/products", (request, response) => {
+  async function fetchProducts() {
+    try {
+      const fetchedData = await fetch(
+        "https://mockend.up.railway.app/api/products/"
+      );
+      const parsedData = await fetchedData.json();
+
+      response.status(200).json(parsedData);
+    } catch (error) {
+      console.error("Error fetching products", error);
+
+      response.status(500).json({ error: "Error fetching products" });
+    }
+  }
+
+  fetchProducts();
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
 });
